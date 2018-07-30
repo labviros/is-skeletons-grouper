1dbcc91aeafdbc4964eac9afaa698d46fc69c481
Felippe <mendonca.felippe@gmail.com>
2018-07-30 16:21:14 +0100

    new features - now service supports any available referencial - new docker version released - publish topic modified to 'Skeletons.Localization'

--- service.cpp
+++ service.cpp
@@ -22,6 +22,22 @@ auto get_id(std::string const& topic) {
     throw std::runtime_error(fmt::format("Wrong fromat topic: \'{}\'", topic));
 }
 
+auto make_frame_transformation(std::string const& topic, is::common::Tensor const& tf) {
+  boost::optional<is::vision::FrameTransformation> maybe_transformation;
+  auto const id_regex = std::regex("FrameTransformation.(\\d+).(\\d+)");
+  std::smatch matches;
+  if (std::regex_match(topic, matches, id_regex)) {
+    if (matches.size() == 3) {
+      is::vision::FrameTransformation transformation;
+      transformation.set_from(std::stoi(matches[1]));
+      transformation.set_to(std::stoi(matches[2]));
+      *transformation.mutable_tf() = tf;
+      maybe_transformation = transformation;
+    }
+  }
+  return maybe_transformation;
+}
+
 auto count_detections(std::unordered_map<int64_t, is::vision::ObjectAnnotations>& sks) {
   std::vector<int64_t> cameras;
   std::transform(sks.begin(), sks.end(), std::back_inserter(cameras), [](auto& kv) { return kv.first; });
@@ -46,12 +62,12 @@ int main(int argc, char** argv) {
   auto subscription = is::Subscription(channel);
 
   ZipkinOtTracerOptions zp_options;
-  zp_options.service_name = "skeletons_grouper";
+  zp_options.service_name = "Skeletons";
   zp_options.collector_host = options.zipkin_host();
   zp_options.collector_port = options.zipkin_port();
   auto tracer = makeZipkinOtTracer(zp_options);
   channel.set_tracer(tracer);
-  auto span_name = fmt::format("group_{}", options.referencial());
+  auto span_name = fmt::format("localizations_{}", options.referencial());
 
   is::vision::GetCalibrationRequest calibs_request;
   *calibs_request.mutable_ids() = options.cameras_ids();
@@ -70,6 +86,35 @@ int main(int argc, char** argv) {
   for (auto& mc : maybe_calibrations->calibrations()) {
     calibrations[mc.id()] = mc;
   }
+  // check for referencial on calibrations
+  std::vector<int64_t> without_ref;
+  for (auto& kv : calibrations) {
+    auto begin = kv.second.extrinsic().begin();
+    auto end = kv.second.extrinsic().end();
+    auto pos = std::find_if(begin, end, [&](auto& ext) { return ext.from() == options.referencial(); });
+    if (pos == end) {
+      auto camera = kv.first;
+      without_ref.push_back(camera);
+      auto tf_topic = fmt::format("FrameTransformation.{}.{}", options.referencial(), camera);
+      subscription.subscribe(tf_topic);
+    }
+  }
+
+  is::info("Waiting for 5 seconds to necessary transformations");
+  auto consume_deadline = system_clock::now() + seconds(5);
+  while (!without_ref.empty()) {
+    auto msg = channel.consume_until(consume_deadline);
+    if (!msg) break;
+    auto maybe_tensor = msg->unpack<is::common::Tensor>();
+    if (!maybe_tensor) continue;
+    auto maybe_transformation = make_frame_transformation(msg->topic(), *maybe_tensor);
+    if (!maybe_transformation) continue;
+    auto camera = maybe_transformation->to();
+    *calibrations[camera].add_extrinsic() = *maybe_transformation;
+    without_ref.erase(std::remove(without_ref.begin(), without_ref.end(), camera), without_ref.end());
+    subscription.unsubscribe(msg->topic());
+  }
+  if (!without_ref.empty()) { is::critical("Can't get all necessary transformations."); }
 
   SkeletonsGrouper grouper(calibrations, options.referencial(), options.min_error());
   for (auto& camera : options.cameras_ids()) {
@@ -105,12 +150,12 @@ int main(int argc, char** argv) {
     auto tf = system_clock::now();
 
     auto sks_message = is::Message(sks_3d);
-    sks_message.set_topic(fmt::format("Skeletons.{}.Detections", options.referencial()));
+    sks_message.set_topic("Skeletons.Localizations");
     sks_message.inject_tracing(tracer, span->context());
     channel.publish(sks_message);
-    
+
     auto n_skeletons = sks_3d.objects().size();
-    span->SetTag("Detections", n_skeletons);
+    span->SetTag("Localizations", n_skeletons);
     span->Finish();
 
     auto dt_ms = duration_cast<microseconds>(tf - t0).count() / 1000.0;
